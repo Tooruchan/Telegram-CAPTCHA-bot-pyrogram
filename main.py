a7c894fd5923288fcab62b666b0f9b91092c0b0e
Kobayashi Tooruchan <39438685+Tooruchan@users.noreply.github.com>
2022-01-24 17:42:26 +0800

    Fixed another bug.

--- main.py
+++ main.py
@@ -350,15 +350,11 @@ def _update(app):
                         permissions=ChatPermissions(
                             can_send_messages=True,
                             can_send_media_messages=True,
-                            can_send_stickers=True,
-                            can_send_animations=True,
-                            can_send_games=True,
-                            can_use_inline_bots=True,
+                            can_send_other_messages=True,
                             can_add_web_page_previews=True,
-                            can_send_polls=True,
-                            can_change_info=True,
-                            can_invite_users=True,
-                            can_pin_messages=True))
+                            can_send_polls=True
+                        )
+                    )
                 except ChatAdminRequired:
                     await client.answer_callback_query(
                         query_id, group_config["msg_bot_no_permission"])
